95a0bc21859bd25c60ef60879a737d5ec696a367
FacundoS <agustinsouyaux@gmail.com>
2023-04-20 19:04:33 -0300

    Incluyendo los dos ejercicios en este commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("---------------------Ejercicio A funciones---------------------");
+        int resultado = suma(5, 5, 5);
+        System.out.println("El resultado de la operacion es: "+ resultado);
+        System.out.println("---------------------Ejercicio B clases---------------------");
         Coche miCoche = new Coche();
         miCoche.agregarPuerta();
         miCoche.getPuertas();
         miCoche.agregarPuerta();
         miCoche.getPuertas();
     }
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
 }
+
+
 class Coche {
     public int puertas = 2;
 
